4a3a90080df09e0a41d25b22fe38510203d4e791
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2018-01-06 21:15:45 +0000

    Finished coding the exercise.

--- users/views.py
+++ users/views.py
@@ -4,7 +4,11 @@ from users.models import User
 
 
 # Create your views here.
-def index(request):
+def users_index(request):
     users = User.objects.order_by('last_name')
     users_dict = {"users_records": users}
     return render(request, "users/index.html", context=users_dict)
+
+
+def index(request):
+    return render(request, "index.html")
